d8de933827077ee940dac9572431e838f9a5054e
riccardofano <rcr.fano@gmail.com>
2022-07-22 11:10:39 +0200

    style header

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,12 +2,16 @@ import Nav from "./Nav";
 
 function Header() {
   return (
-    <header className="container flex flex-v-center">
-      <img src="images/logo.svg" alt="Sneakers logo" />
-      <Nav />
-      <div>
+    <header className="header | container flex flex-v-center flex-sb">
+      <div className="flow-h | flex flex-v-center">
+        <img src="images/logo.svg" alt="Sneakers logo" />
+        <Nav />
+      </div>
+      <div className="flow-h | flex flex-v-center max-100">
         <img src="images/icon-cart.svg" alt="Shopping cart" />
-        <img src="images/image-avatar.png" alt="Profile avatar" />
+        <div className="img-wrapper">
+          <img src="images/image-avatar.png" alt="Profile avatar" />
+        </div>
       </div>
     </header>
   );
