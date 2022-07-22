0f10bd19df45b8277c6ea34c1857532f526b46eb
riccardofano <rcr.fano@gmail.com>
2022-07-22 17:58:26 +0200

    make reducers pure

--- src/context/reducers.ts
+++ src/context/reducers.ts
@@ -1,26 +1,33 @@
 import { Product, Shop } from "./shopContext";
 
-// Adds specified amount of products
 const addToCart = (product: Product, amount: number, state: Shop): Shop => {
   const cart = [...state.cart];
   const itemIndex = cart.findIndex((item) => item.id === product.id);
 
-  if (itemIndex > 0) {
-    cart[itemIndex].amount += amount;
-  } else {
+  if (itemIndex < 0) {
     cart.push({ ...product, amount });
+  } else {
+    const item = { ...cart[itemIndex] };
+    item.amount += amount;
+    cart[itemIndex] = item;
   }
 
   return { ...state, cart };
 };
 
-// Removes product regardless of the quantity
 const removeFromCart = (productId: string, state: Shop): Shop => {
   const cart = [...state.cart];
   const itemIndex = cart.findIndex((item) => item.id === productId);
 
-  if (itemIndex > 0) {
+  if (itemIndex < 0) return { ...state };
+
+  const item = { ...cart[itemIndex] };
+  item.amount -= 1;
+
+  if (item.amount === 0) {
     cart.splice(itemIndex, 1);
+  } else {
+    cart[itemIndex] = item;
   }
 
   return { ...state, cart };
