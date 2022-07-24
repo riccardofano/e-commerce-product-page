18fdb6948b7184841dae2b8772ebb2c82b12a68c
riccardofano <rcr.fano@gmail.com>
2022-07-24 16:02:15 +0200

    finishing touches

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -30,18 +30,18 @@ function Main() {
     <main className={`main | grid even-columns ${isDesktop ? "container" : ""}`}>
       <Gallery images={images} />
       <article className={`flow ${!isDesktop ? "container" : ""}`}>
-        <p className="text-primary-400 text-uppercase fw-bold fs-xs">{brand}</p>
-        <h1 className="mg-sm text-neutral-800 fw-bold fs-xl">{name}</h1>
-        <p className="fs-sm">{description}</p>
+        <p className="brand | text-primary-400 text-uppercase fw-bold fs-xs ls-lg">{brand}</p>
+        <h1 className="mg-sm text-neutral-800 fw-bold fs-xl lh-sm ls-xs">{name}</h1>
+        <p className="description | fs-sm lh-lg">{description}</p>
         <div className="price | flex flex-v-center flex-sb fw-bold">
           <div className="flow-h | flex flex-v-center">
-            <p className="text-neutral-800 fs-lg">${(price / 100).toFixed(2)}</p>
+            <p className="text-neutral-800 fs-lg ls-sm">${(price / 100).toFixed(2)}</p>
             {discount && <span className="pill | text-primary-400 bg-primary-200 fs-sm">{discount}%</span>}
           </div>
           {oldPrice && <p className="text-neutral-400 text-strike fs-sm">${(oldPrice / 100).toFixed(2)}</p>}
         </div>
         <div className="grid uneven-columns">
-          <div className="flex flex-v-center flex-sb bg-neutral-300">
+          <div className="flex flex-v-center flex-sb bg-neutral-300 radius-lg">
             <button className="button | flex flex-v-center" onClick={descreaseAmount}>
               <IconMinus />
             </button>
@@ -50,7 +50,12 @@ function Main() {
               <IconPlus />
             </button>
           </div>
-          <button className="button | flow-h | flex flex-center" data-type="primary" onClick={addProductToCart}>
+          <button
+            className="button | flow-h | flex flex-center"
+            data-type="primary"
+            data-shadow="true"
+            onClick={addProductToCart}
+          >
             <IconCart />
             <p className="fw-bold">Add to cart</p>
           </button>
